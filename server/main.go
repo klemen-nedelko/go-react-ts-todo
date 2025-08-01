da7838148624c62978ed334b0299254eeb01f6fc
klemen <klemen.nedelko2@gmail.com>
2025-08-01 10:49:01 +0200

    todo app with go and react ts

---
+++ server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type TODO struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Done  bool   `json:"done"`
+}
+
+func main() {
+	fmt.Print("Hello, World!")
+
+	app := fiber.New()
+
+	todos := []TODO{}
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &TODO{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.JSON(todos)
+	})
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+	log.Fatal(app.Listen(":4000"))
+
+}
